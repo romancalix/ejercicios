ecf38dd46b86dd2ea2f32c60ff0d0fd8853ea604
Luis <roman.calix.net@gmail.com>
2026-01-26 22:50:50 -0600

    Anagrama

---
+++ InterviewTest/Strategies/Anagrama.cs
@@ -0,0 +1,37 @@
+﻿namespace InterviewTest.Strategies
+{
+ /*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+    public class Anagrama : IStrategy
+    {
+        private string _word1;
+        private string _word2;
+        public Anagrama()
+        {
+            this._word1 = "roma";
+            this._word2 = "amor";
+        }
+        public void Execute()
+        {
+            for (int i = 0; i <= this._word1.Length - 1; i++)
+            {
+                this._word2 = this._word2.Replace(this._word1[i].ToString(), "");
+            }
+
+            if (this._word1 != this._word2 && this._word2.Length == 0)
+            {
+                Console.WriteLine("Las palabras son anagramas");
+            }
+            else
+            {
+                Console.WriteLine("Las palabras no son anagramas");
+            }
+        }
+    }
+}
