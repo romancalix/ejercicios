df04699e9bb175e1eba076e7547a8f9cfb7fa3f9
Luis <roman.calix.net@gmail.com>
2026-02-02 14:36:51 -0600

    decimal to binary strategy

---
+++ InterviewTest/Strategies/DecimalBinario.cs
@@ -0,0 +1,59 @@
+﻿
+namespace InterviewTest.Strategies
+{
+    /*
+     * Crea un programa se encargue de transformar un número
+     * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+     */
+
+    /**
+     Método 1: División sucesiva por 2 (Recomendado)
+        Pasos:
+
+        Dividir el número decimal entre 2
+
+        Anotar el cociente y el resto (0 o 1)
+
+        Repetir con el cociente hasta que sea 0
+
+        Leer los restos de abajo hacia arriba
+
+        Ejemplo: Convertir 13₁₀ a binario
+
+        text
+        13 ÷ 2 = 6  resto 1  ↑
+         6 ÷ 2 = 3  resto 0  |
+         3 ÷ 2 = 1  resto 1  | Leer de ABAJO hacia ARRIBA
+         1 ÷ 2 = 0  resto 1  ↓
+     */
+    public class DecimalBinario : IStrategy<string>
+    {
+        private readonly int _number;
+        public string binary { get; private set; }
+
+        public DecimalBinario(int number)
+        {
+            _number = number;
+        }
+
+        public string Response => binary;
+
+        public void Execute()
+        {
+            int rest = 1;
+            int num = 1;
+            int number = this._number;
+
+            while (num > 0) 
+            {
+            
+                rest = number % 2;
+                num = number / 2;
+                binary = rest.ToString() + binary;
+                number = num;
+
+
+            }
+        }
+    }
+}
---
+++ TestManager.Test/DecimalBinaryTest.cs
@@ -0,0 +1,30 @@
+﻿
+
+using InterviewTest;
+using InterviewTest.Strategies;
+
+namespace TestManager.Test
+{
+    public class DecimalBinaryTest
+    {
+        [Theory]
+        [InlineData(10, "1010")]
+        [InlineData(0, "0")]
+        [InlineData(255, "11111111")]
+        [InlineData(1, "1")]
+        [InlineData(13, "1101")]
+        public void DecimalToBinaryTest(int number, string binaryResult)
+        {
+            // Arrange
+            IStrategy<string> strategy = new DecimalBinario(number);
+
+            // Act
+            strategy.Execute();
+            var result = strategy.Response;
+            
+            // Assert
+            Assert.Equal(binaryResult, result);
+
+        }
+    }
+}
