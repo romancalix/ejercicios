c1ebe5b29e66d6407f1d841e82693ef9d937d797
Luis <roman.calix.net@gmail.com>
2026-05-20 12:38:20 -0600

    tests completed successfully

---
+++ InterviewTest/Strategies/BalancedBrackets.cs
@@ -0,0 +1,76 @@
+﻿namespace InterviewTest.Strategies
+{
+
+    /**
+     1. Balanced Brackets
+        Description:
+        Given a string containing only '(', ')', '{', '}', '[', ']', determine if the input string is balanced. A string is balanced if:
+
+        Every opening bracket has a corresponding closing bracket of the same type.
+
+        Brackets are closed in the correct order.
+
+        Input Format:
+        A single string s (1 ≤ |s| ≤ 10^4).
+
+        Output Format:
+        Print "YES" if balanced, otherwise "NO".
+
+        Sample Input:
+        {[()]}
+
+        Sample Output:
+        YES
+     */
+    public class BalancedBrackets : IStrategy<bool>
+    {
+        private readonly string input;
+
+        public bool Response { get; private set; }
+
+        public BalancedBrackets(string input)
+        {
+            this.input = input;
+        }
+
+        public void Execute()
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> charPairs = new Dictionary<char, char>()
+            {
+                { '(', ')' },
+                { '{', '}' },
+                { '[', ']' }
+            };
+            foreach (char c in input) 
+            {
+                if (charPairs.ContainsKey(c))
+                { 
+                    stack.Push(c);
+                } else if (charPairs.ContainsValue(c) ) 
+                {
+                    if (stack.Count <= 0) 
+                    {
+                        this.Response = false;
+                        return;
+                    }
+
+                    char lastChar = stack.Peek();
+                    char keyValue = charPairs.FirstOrDefault(x => x.Value == c).Key;
+
+                    if (lastChar == keyValue)
+                    {
+                        stack.Pop();
+                    }
+                    else 
+                    {
+                        this.Response = false;
+                        return;
+                    }
+
+                }
+            }
+            this.Response = stack.Count == 0;
+        }
+    }
+}
---
+++ TestManager.Test/BalancedBracketsTest.cs
@@ -0,0 +1,36 @@
+﻿using InterviewTest;
+using InterviewTest.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestManager.Test
+{
+    public class BalancedBracketsTest
+    {
+        [Theory]
+        [InlineData("{[()]}", true)]
+        [InlineData("({[]})[]()", true)]
+        [InlineData("{[(])}", false)]
+        [InlineData("()(", false)]
+        [InlineData("())", false)]
+        [InlineData("[[[{{(", false)]
+        [InlineData("{[)(]}", false)]
+        [InlineData("[{(})]", false)]
+        [InlineData(")(", false)]
+        public void TestBalancedBrackets(string input, bool response) 
+        {
+            //arrange
+            IStrategy<bool> strategy = new BalancedBrackets(input);
+            var context = new Context<bool>(strategy);
+
+            //act
+            context.ExecuteStrategy();
+
+            //assert
+            Assert.Equal(response, strategy.Response);
+        }
+    }
+}
