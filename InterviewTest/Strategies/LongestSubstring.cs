618f61638f614357b85e76a7d690a5cef2b59618
Luis <roman.calix.net@gmail.com>
2026-05-20 23:53:04 -0600

    LongestSubstring

---
+++ InterviewTest/Strategies/LongestSubstring.cs
@@ -0,0 +1,41 @@
+﻿namespace InterviewTest.Strategies
+{
+    public class LongestSubstring : IStrategy<int>
+    {
+        public int Response { get; set; }
+        private readonly string _input;
+
+        public LongestSubstring(string input)
+        {
+            _input = input;
+        }
+
+        public void Execute()
+        {
+            int mayor = 0;
+            int count = 0;
+            Stack<char> cadena = new Stack<char>();
+            foreach (var item in _input) 
+            {
+
+                for (int i = count; i < this._input.Count(); i++)
+                {
+                    if (cadena.Contains(this._input[i]))
+                    {
+                        mayor = mayor > cadena.Count ? mayor : cadena.Count;
+                        cadena.Clear();
+                        break;
+                    }
+                    else
+                    {
+                        cadena.Push(this._input[i]);
+                    }
+                }
+
+                count++;
+            }
+
+            this.Response = mayor;
+        }
+    }
+}
---
+++ TestManager.Test/LongestSubstringTest.cs
@@ -0,0 +1,48 @@
+﻿using InterviewTest;
+using InterviewTest.Strategies;
+
+namespace TestManager.Test
+{
+    public class LongestSubstringTest
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData("abcdef", 6)]
+        [InlineData("abba", 2)]
+        [InlineData("dvdf", 3)]  // Este suele fallar en implementaciones incorrectas
+        [InlineData("tmmzuxt", 5)]  // Clásico caso tricky
+        public void LongestSubsTest(string cad, int val) 
+        {
+            //arrange
+            IStrategy<int> strategy = new LongestSubstring(cad);
+            var context = new Context<int>(strategy);
+
+            //act
+            context.ExecuteStrategy();
+            int response = context.GetResponse();
+
+            //assert
+            Assert.Equal(val, response);
+
+        }
+
+        [Theory]
+        [InlineData("dvdf", 3)]
+        [InlineData("tmmzuxt", 5)]
+        [InlineData("abba", 2)]
+        public void VerificacionDirecta(string cad, int val)
+        {
+            // Sin usar Context, directamente la estrategia
+            var strategy = new LongestSubstring(cad);
+            strategy.Execute();
+
+            Console.WriteLine($"Input: {cad}");
+            Console.WriteLine($"Resultado obtenido: {strategy.Response}");
+            Console.WriteLine($"Resultado esperado: {val}");
+
+            Assert.Equal(val, strategy.Response);
+        }
+    }
+}
